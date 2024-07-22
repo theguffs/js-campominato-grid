f2ab47ee6d478df8065ee4468327224a514adc01
theguffs <youssefabdelmalak01@gmail.com>
2024-07-22 16:10:19 +0200

    bonus fatto

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 document.getElementById('generate-button').addEventListener('click', function() {
     const container = document.getElementById('grid-container');
+    const difficulty = document.getElementById('difficoltà').value;
 // Pulisce il contenitore prima di aggiungere nuove celle
-    container.innerHTML = ''; 
-    for (let i = 1; i <= 100; i++) {
-        const quadrati = document.createElement('div');
-        quadrati.classList.add('quadrati');
-        quadrati.textContent = i;
-        quadrati.addEventListener('click', function() {
-            quadrati.style.backgroundColor = 'lightcoral';
+    container.innerHTML = '';
+
+    let quadratitot;
+    let livello;
+
+    if (difficulty == 'difficile') {
+        quadratitot = 100;
+        livello = 'difficile';
+    } 
+    else if (difficulty == 'medio') {
+        quadratitot = 81;
+        livello = 'medio';
+    } 
+    else if (difficulty == 'facile') {
+        quadratitot = 49;
+        livello = 'facile';
+    }
+
+    for (let i = 1; i <= quadratitot; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('quadrati', livello);
+        cell.textContent = i;
+        cell.addEventListener('click', function() {
+            cell.style.backgroundColor = 'lightcoral';
             console.log(`Cella cliccata: ${i}`);
         });
-        container.append(quadrati);
+        container.append(cell);
     }
 });
